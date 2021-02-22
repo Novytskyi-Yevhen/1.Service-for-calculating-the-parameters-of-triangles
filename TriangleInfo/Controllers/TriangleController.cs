e3b4dcb41c49b62ec5592504d6a09efec4c1e169
Евгений Новицкий <serverdog1@gmail.com>
2021-02-22 20:04:57 +0200

    Add action Info, add link to open Info action

--- TriangleInfo/Controllers/TriangleController.cs
+++ TriangleInfo/Controllers/TriangleController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using TriangleInfo.Models;
+using System;
 
 namespace TriangleInfo.Controllers
 {
@@ -18,9 +19,19 @@ namespace TriangleInfo.Controllers
             _logger = logger;
         }
 
-        public string Ret(int id)
+        public string Info(int side1, int side2, int side3)
         {
-            return $"{id}";
+            double perimeter = side1 + side2 + side3;
+            double semiPerimeter = perimeter / 2;
+            double areaTriangle = Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+            return "------------------------------------------------------------" + "\n" +
+                "Triangle:" + "\n" +
+                $"({side1}, {side2}, {side3})" + "\n" +
+                "Reduced:" + "\n" +
+                $"({side1 / perimeter}, {side2 / perimeter}, {side3 / perimeter})" + "\n" +
+                $"Area = {areaTriangle}" + "\n" +
+                $"Perimeter = {perimeter}" + "\n" +
+                "------------------------------------------------------------";
         }
        
     }
