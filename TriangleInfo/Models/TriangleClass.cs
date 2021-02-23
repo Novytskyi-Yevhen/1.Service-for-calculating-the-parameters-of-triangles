9dae1b872219df2779f93fac2482052ce194e3a6
Lilya Karas <lilyca.hrupa@gmail.com>
2021-02-23 11:10:44 +0200

    edit equals, add operators ==, !=

--- TriangleInfo/Models/TriangleClass.cs
+++ TriangleInfo/Models/TriangleClass.cs
@@ -13,7 +13,18 @@ namespace TriangleInfo.Controllers
 
         public override bool Equals(object obj)
         {
+            if (obj != null && obj is TriangleClass second)
+                return this.side1 == second.side1 && this.side2 == second.side2 && this.side3==second.side3;
+
             return this.GetHashCode() == obj.GetHashCode();    
+        }
+        public static bool operator ==(TriangleClass a, TriangleClass b)
+        {
+            return a.Equals(b);
+        }
+        public static bool operator !=(TriangleClass a, TriangleClass b)
+        {
+            return !(a == b);
         }
         public override int GetHashCode()
         {
